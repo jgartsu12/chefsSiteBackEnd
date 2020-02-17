fa6e4204d084c4c84aa486e3f194849239425230
jgartsu12 <jgartsu12@gmail.com>
2020-02-17 12:17:44 -0500

    fixed foods model

--- src/foods/models.py
+++ src/foods/models.py
@@ -2,11 +2,30 @@ from django.db import models
 
 class Food(models.Model):
     title = models.CharField(max_length=120)
-    category = models.CharField(max_length=20)
+    SOUPS = 'SOUPS'
+    WRAPS = 'WRAPS'
+    BURITTOS = 'BURITTOS'
+    PANINIS = 'PANINIS'
+    BREAKFAST_SANDWHICHES = 'BREAKFAST SANDWHICHES'
+    HOT_DRINKS = 'HOT DRINKS'
+    GRILLED_CHEESES = 'GRILLED CHEESES'
+    CATEGORY_CHOICES = [
+        (SOUPS, 'SOUPS'),
+        (WRAPS, 'WRAPS'),
+        (BURITTOS, 'BURITTOS'),
+        (PANINIS, 'PANINIS'),
+        (BREAKFAST_SANDWHICHES, 'BREAKFAST SANDWHICHES'),
+        (HOT_DRINKS, 'HOT DRINKS'),
+        (GRILLED_CHEESES, 'GRILLED CHEESES'),
+    ]
+    category = models.CharField(
+        max_length=150,
+        choices=CATEGORY_CHOICES,
+        default=SOUPS,
+    )
     content = models.TextField()
-    description = models.CharField(max_length=120, default="", editable=False)
-    price = models.DecimalField(max_digits=10,decimal_places=2)
-
+    description = models.CharField(max_length=120)
 
     def __str__(self):
         return self.title
+
